ae7a231e1947e7e38cfc6fbd339b7fc06afd309e
Artyom <artem.kolosov2000@gmail.com>
2021-03-01 21:37:41 +0300

    add doc

--- Lab1/src/main/java/functions/MultiModalFunction.java
+++ Lab1/src/main/java/functions/MultiModalFunction.java
@@ -2,8 +2,14 @@ package functions;
 
 import interfaces.MathFunction;
 
+/**
+ * Класс многомодальной функции
+ */
 public class MultiModalFunction implements MathFunction {
 
+    /**
+     * {@link MathFunction#run(double)}
+     */
     @Override
     public double run(double x) {
         return x * Math.sin(x) - Math.cos(x);
--- Lab1/src/main/java/interfaces/Search.java
+++ Lab1/src/main/java/interfaces/Search.java
@@ -1,5 +1,12 @@
 package interfaces;
 
+/**
+ * Интерфейс поисковика
+ */
 public interface Search {
+    /**
+     * Метод поиска точки минимума функции
+     * @return - тока, где достигается минимум на функции
+     */
     double searchMinimum();
 }
