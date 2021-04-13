fa866e673567a6c27002780266491a01fb73087c
Dmitriy Korobkov <dima.korobkovv@gmail.com>
2021-04-13 17:12:11 +0300

    main

--- Lab2/src/main/java/Main.java
+++ Lab2/src/main/java/Main.java
@@ -1,20 +1,23 @@
 import Functions.QuadraticFunction;
+import methods.GradientDescentMethod;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         double[][] a = new double[][]{
-                {8, 6},
-                {6, 8}
+                {128, 12},
+                {12, 128}
         };
         double[] b = new double[]{-10, 30};
         double c = 13;
         QuadraticFunction f = new QuadraticFunction(a, b, c);
-        System.out.println(f.run(new double[]{0.1, -0.24}));
-        /*System.out.println(
+        System.out.println(f.run(new double[]{1, 1}));
+        System.out.println(
                 Arrays.toString(
                         new GradientDescentMethod(0.1, 0.00001)
                                 .findMinimum(f, new double[]{1, 1})
                 )
-        );*/
+        );
     }
 }
