b5abe1486c7b4798aa0c065a14ce7b5d83d921d3
Dmitriy Korobkov <dima.korobkovv@gmail.com>
2021-04-14 23:15:51 +0300

    fixes

--- Lab2/src/main/java/Main.java
+++ Lab2/src/main/java/Main.java
@@ -1,5 +1,6 @@
 import Functions.QuadraticDiagonalFunction;
 import Functions.QuadraticFunction;
+import interfaces.Function;
 import methods.ConjugateGradientMethod;
 import methods.FastGradientDescentMethod;
 import methods.GradientDescentMethod;
@@ -15,7 +16,7 @@ public class Main {
         double[] b = new double[]{10, -30};
         double c = 13;
         QuadraticFunction f = new QuadraticFunction(a, b, c);
-        System.out.println("real = 13; cur = " + f.run(new double[]{0, 0}));
+        /*System.out.println("real = 13; cur = " + f.run(new double[]{0, 0}));
         System.out.println("real = 173; cur = " + f.run(new double[]{1, 1}));
         System.out.println("real = 8.83; cur = " + f.run(new double[]{0.1, -0.24}));
         System.out.println(
@@ -23,30 +24,30 @@ public class Main {
                         new GradientDescentMethod(0.1, 0.00001)
                                 .findMinimum(f, new double[]{1, 1})
                 )
-        );
-        System.out.println(
+        );*/
+        /*System.out.println(
                 Arrays.toString(
                         new FastGradientDescentMethod(0.00001)
                                 .findMinimum(f, new double[]{1, 1})
                 )
-        );
-        System.out.println(
+        );*/
+        /*System.out.println(
                 Arrays.toString(
                         new ConjugateGradientMethod(0.00001)
                                 .findMinimum(f, new double[]{1, 1})
                 )
-        );
+        );*/
         double[] aDiag = new double[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         double[] bDiag = new double[]{-1 ,-1, -1, -1, -1, -1, -1, -1, -1, -1};
-        double cDiag = 0;
+        double cDiag = 0; // a^2 + a + b^2 + b + ... + j^2 + j
         QuadraticDiagonalFunction fDiag = new QuadraticDiagonalFunction(aDiag, bDiag, cDiag);
 //        System.out.println("real = 13; cur = " + fDiag.run(new double[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}));
 //        System.out.println("real = 173; cur = " + fDiag.run(new double[]{1, 1, 1 ,1, 1, 1, 1, 1, 1, 1}));
 //        System.out.println("real = 8.83; cur = " + fDiag.run(new double[]{0.1, -0.24}));
-        System.out.println(
+        /*System.out.println(
                 Arrays.toString(
-                        new GradientDescentMethod(0.5, 0.00001)
-                                .findMinimum(fDiag, new double[]{1, 1, 1 ,1, 1, 1, 1, 1, 1, 1})
+                        new GradientDescentMethod(0.0001, 0.00001)
+                                .findMinimum(fDiag, new double[]{0,0,0,0,0,0,0,0,0,0})
                 )
         );
         System.out.println(
@@ -59,6 +60,13 @@ public class Main {
                 Arrays.toString(
                         new ConjugateGradientMethod(0.00001)
                                 .findMinimum(fDiag, new double[]{1, 1, 1 ,1, 1, 1, 1, 1, 1, 1})
+                )
+        );*/
+
+        Function f1 = new QuadraticDiagonalFunction(new double[]{3, 7}, new double[]{0, 1}, 0);
+        System.out.println(
+                Arrays.toString(
+                        new FastGradientDescentMethod(0.00001).findMinimum(f1, new double[]{-1, -2})
                 )
         );
     }
--- Lab2/src/main/java/methods/FastGradientDescentMethod.java
+++ Lab2/src/main/java/methods/FastGradientDescentMethod.java
@@ -3,13 +3,14 @@ package methods;
 import interfaces.Function;
 import interfaces.MathFunction;
 import interfaces.Method;
-import search.BrentSearch;
+import search.ParabolSearch;
 
 
 import static SaZhaK.MatrixUtil.*;
 
 public class FastGradientDescentMethod implements Method {
     private final double epsilon;
+    private int counter = 0;
 
     public FastGradientDescentMethod(final double epsilon) {
         this.epsilon = epsilon;
@@ -24,12 +25,14 @@ public class FastGradientDescentMethod implements Method {
                 break;
             }
             x = subtract(x, multiply(gradient, calculateStep(x, gradient, function)));
+            counter++;
         }
+        System.out.println("minimum iterations: " + counter);
         return x;
     }
 
     private double calculateStep(double[] x, double[] gradient, Function function) {
         MathFunction fun = alpha -> function.run(subtract(x, multiply(gradient, alpha)));
-        return new BrentSearch(fun,0, 10, epsilon).searchMinimum();
+        return new ParabolSearch(fun,-10, 10, epsilon).searchMinimum();
     }
 }
