b88627cb2cf74819cd4849b18209802df95b54ab
ADevil000 <artem.kolosov2000@yandex.ru>
2021-05-13 20:06:09 +0300

    add parser

--- Lab3.1/src/generator/MatrixGenerator.java
+++ Lab3.1/src/generator/MatrixGenerator.java
@@ -1,13 +1,23 @@
 package generator;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class MatrixGenerator {
-    public static Random random;
 
-    public static double[][] generateOrdinaryMatrix(int dimension, int k) {
-        double[][] matrix = new double[dimension][dimension];
-        int[] profileOffset = new int[dimension];
+    public static Random random;
+    private static final String[] NAME_OF_FILES = {"au.txt", "al.txt", "ia.txt", "d.txt"};
+
+    public static double[][] generateOrdinaryMatrix(final int dimension, final int k) {
+        final double[][] matrix = new double[dimension][dimension];
+        final int[] profileOffset = new int[dimension];
 
         for (int i = 0; i < dimension; ++i) {
             profileOffset[i] = random.nextInt(i + 1);
@@ -41,18 +51,53 @@ public class MatrixGenerator {
         return matrix;
     }
 
-    public static double[][] generateHilbertMatrix(int dimension) {
-        double[][] matrix = new double[dimension][dimension];
+    public static double[][] generateHilbertMatrix(final int dimension) {
+        final double[][] matrix = new double[dimension][dimension];
         for (int i = 1; i <= dimension; ++i) {
             for (int j = 1; j <= dimension; ++j) {
                 matrix[i - 1][j - 1] = (double) 1 / (i + j - 1);
             }
         }
 
+
         return matrix;
     }
 
-    public static void parse() {
-
+    public static void parseAndWrite(final double[][] matrix, final String... path) {
+        final int size = matrix.length;
+        final double[] d = new double[size];
+        final ArrayList<Double> al = new ArrayList<>();
+        final ArrayList<Double> au = new ArrayList<>();
+        final int[] ia = new int[size + 1];
+        for (int i = 0; i < size; i++) {
+            d[i] = matrix[i][i];
+        }
+        ia[0] = 0;
+        ia[1] = 0;
+        for (int i = 1; i < size; i++) {
+            int ind = 0;
+            while (ind < i && matrix[i][ind] == 0) {
+                ind++;
+            }
+            while (ind < i) {
+                al.add(matrix[i][ind]);
+                au.add(matrix[ind][i]);
+                ind++;
+            }
+            ia[i + 1] = al.size();
+        }
+        for (final String fileName : NAME_OF_FILES) {
+            try (final BufferedWriter out = Files.newBufferedWriter(Path.of(String.join("", path), fileName))) {
+                switch (fileName) {
+                    case "au.txt" -> out.write(au.stream().map(Object::toString).collect(Collectors.joining(" ")));
+                    case "al.txt" -> out.write(al.stream().map(Object::toString).collect(Collectors.joining(" ")));
+                    case "ia.txt" -> out.write(Arrays.stream(ia).mapToObj(Objects::toString).collect(Collectors.joining(" ")));
+                    case "d.txt" -> out.write(Arrays.stream(d).mapToObj(Objects::toString).collect(Collectors.joining(" ")));
+                }
+            } catch (final IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
+
 }
