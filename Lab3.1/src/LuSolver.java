2bcde9889663a5bf4410bf78b3e1c827b582dfda
Karile Myrr <kirillant2013@gmail.com>
2021-05-19 20:53:26 +0300

    Doc for LUSolver

--- Lab3.1/src/LuSolver.java
+++ Lab3.1/src/LuSolver.java
@@ -1,6 +1,18 @@
 import matrix.ProfileMatrix;
 
+
+/**
+ * Class for solving SoLE by Gauss with LU decomposition
+ */
 public class LuSolver {
+
+    /**
+     * Решение СЛАУ (Ах = b) с нижнетрегугольной матрицей методом Гаусса, прямого хода.
+     * Ответ находится в векторе b
+     *
+     * @param matrix {@link ProfileMatrix} - левая часть уровнения, нижнетреугольная матрица.
+     * @param b - правая часть уровнения.
+     */
     private static void gaussForward(final ProfileMatrix matrix, final double[] b) {
         for (int i = 0; i < matrix.size(); ++i) {
             for (int j = 0; j < i; ++j) {
@@ -10,6 +22,13 @@ public class LuSolver {
         }
     }
 
+    /**
+     * Решение СЛАУ (Ах = y) с верхнетреугольной матрицей методом Гаусса, обратного хода.
+     * Ответ находится в векторе b
+     *
+     * @param matrix {@link ProfileMatrix} - левая часть уровнения, верхнетреугольная матрица матрица.
+     * @param y - правая часть уровнения.
+     */
     private static void gaussBackward(final ProfileMatrix matrix, final double[] y) {
         for (int i = matrix.size() - 1; i >= 0; --i) {
             for (int j = matrix.size() - 1; j > i; --j) {
@@ -19,6 +38,14 @@ public class LuSolver {
         }
     }
 
+
+    /**
+     * Решение СЛАУ (Ах = y) через LU разложение + метод Гаусса.
+     * Ответ находится в векторе b
+     *
+     * @param matrix {@link ProfileMatrix} - левая часть уровнения
+     * @param b - правая часть уровнения.
+     */
     public static void solve(final ProfileMatrix matrix, final double[] b) {
 //        System.out.println("matrix");
 //        matrix.showByGetters();
