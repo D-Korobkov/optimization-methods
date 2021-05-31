863028a0b2c765751c0c5c7cfe3846fae71cdc20
Karile Myrr <kirillant2013@gmail.com>
2021-05-31 22:27:39 +0300

    Added tests for classicNewtone

--- Lab4/src/main/java/Main.java
+++ Lab4/src/main/java/Main.java
@@ -8,20 +8,8 @@ import java.util.Arrays;
 
 public class Main {
 
-    private static void classicNewtoneChecker() throws IOException {
-        Method classicNewtone = new ClassicNewtoneMethod();
-        Function f1 = new QuadraticFunction(new double[][]{{2, 0}, {0, 2}}, new double[]{0, 0}, 0);
-        Function f2 = new QuadraticFunction(new double[][]{{20, 20}, {20, 40}}, new double[]{10, 10}, 10);
-
-        System.out.println("Real = [0, 0] :: Ans = " + Arrays.toString(classicNewtone.findMinimum(f1, new double[]{2, 2})));
-        System.out.println("Real = [0, 0] :: Ans = " + Arrays.toString(classicNewtone.findMinimum(f1, new double[]{2, 2})));
-        System.out.println("Real = [0.5, 0] :: Ans = " + Arrays.toString(classicNewtone.findMinimum(f2, new double[]{2, 2})));
-        System.out.println("Real = [0.5, 0] :: Ans = " + Arrays.toString(classicNewtone.findMinimum(f2, new double[]{2, 2})));
-    }
-
     public static void main(String[] args) throws IOException {
 
-        classicNewtoneChecker();
 
     }
 }
---
+++ Lab4/src/test/java/NewtoneTests.java
@@ -0,0 +1,54 @@
+import NewtonMethods.ClassicNewtoneMethod;
+import functions.QuadraticFunction;
+import interfaces.Function;
+import interfaces.Method;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class NewtoneTests {
+
+    @BeforeClass
+    public static void prepare() {
+
+        System.out.println("prepare");
+    }
+
+    @Before
+    public void preTest() {
+
+        System.out.println("run test");
+    }
+
+    @Test
+    public void classicNewtoneTest() throws IOException {
+
+        System.out.println("run classicNewtoneTest");
+
+        double[] x0 = {10, 10};
+        Method classicNewtone = new ClassicNewtoneMethod();
+
+        Function f1 = new QuadraticFunction(new double[][]{{2, 0}, {0, 2}}, new double[]{0, 0}, 0);
+        double[] f1Real = {0, 0};
+        double[] f1Answer = classicNewtone.findMinimum(f1, x0);
+
+        assertArrayEquals("Incorrect minimum in f1", f1Real, f1Answer, 0.0);
+        System.out.println("f1 test passed");
+
+        Function f2 = new QuadraticFunction(new double[][]{{20, 20}, {20, 40}}, new double[]{10, 10}, 10);
+        double[] f2Real = {0.5, 0};
+        double[] f2Answer = classicNewtone.findMinimum(f2, x0);
+
+        assertArrayEquals("Incorrect minimum in f2", f2Real, f2Answer, 0.0);
+        System.out.println("f2 test passed");
+
+        System.out.println("classicNewtone test passed");
+
+    }
+    
+}
