0ccba45ce87d97287cdb0636dffc5fc0c5f8d3e1
Artyom <artem.kolosov2000@gmail.com>
2021-05-31 16:06:24 +0300

    lalal

--- Lab2/src/main/java/Main.java
+++ Lab2/src/main/java/Main.java
@@ -2,80 +2,30 @@ import Functions.HandlerForFunctions;
 import Functions.QuadraticDiagonalFunction;
 import Functions.QuadraticFunction;
 import interfaces.Function;
+import methods.AbstractGradientMethod;
 import methods.ConjugateGradientMethod;
 import methods.FastGradientDescentMethod;
 import methods.GradientDescentMethod;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.DoubleStream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-        HandlerForFunctions handler = new HandlerForFunctions();
-
-        handler.printAll(System.out);
-
-
-        /*double[][] a = new double[][]{
-                {128, 12},
-                {12, 128}
-        };
-        double[] b = new double[]{10, -30};
-        double c = 13;
-        QuadraticFunction f = new QuadraticFunction(a, b, c);
-        /*System.out.println("real = 13; cur = " + f.run(new double[]{0, 0}));
-        System.out.println("real = 173; cur = " + f.run(new double[]{1, 1}));
-        System.out.println("real = 8.83; cur = " + f.run(new double[]{0.1, -0.24}));
-        System.out.println(
-                Arrays.toString(
-                        new GradientDescentMethod(0.1, 0.00001)
-                                .findMinimum(f, new double[]{1, 1})
-                )
-        );*/
-        /*System.out.println(
-                Arrays.toString(
-                        new FastGradientDescentMethod(0.00001)
-                                .findMinimum(f, new double[]{1, 1})
-                )
-        );*/
-        /*System.out.println(
-                Arrays.toString(
-                        new ConjugateGradientMethod(0.00001)
-                                .findMinimum(f, new double[]{1, 1})
-                )
-        );*/
-        double[] aDiag = new double[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        double[] bDiag = new double[]{-1 ,-1, -1, -1, -1, -1, -1, -1, -1, -1};
-        double cDiag = 0; // a^2 + a + b^2 + b + ... + j^2 + j
-        QuadraticDiagonalFunction fDiag = new QuadraticDiagonalFunction(aDiag, bDiag, cDiag);
-//        System.out.println("real = 13; cur = " + fDiag.run(new double[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}));
-//        System.out.println("real = 173; cur = " + fDiag.run(new double[]{1, 1, 1 ,1, 1, 1, 1, 1, 1, 1}));
-//        System.out.println("real = 8.83; cur = " + fDiag.run(new double[]{0.1, -0.24}));
-        /*System.out.println(
-                Arrays.toString(
-                        new GradientDescentMethod(0.0001, 0.00001)
-                                .findMinimum(fDiag, new double[]{0,0,0,0,0,0,0,0,0,0})
-                )
-        );
-        System.out.println(
-                Arrays.toString(
-                        new FastGradientDescentMethod(0.00001)
-                                .findMinimum(fDiag, new double[]{1, 1, 1 ,1, 1, 1, 1, 1, 1, 1})
-                )
-        );
-        System.out.println(
-                Arrays.toString(
-                        new ConjugateGradientMethod(0.00001)
-                                .findMinimum(fDiag, new double[]{1, 1, 1 ,1, 1, 1, 1, 1, 1, 1})
-                )
-        );*/
-
-        Function f1 = new QuadraticDiagonalFunction(new double[]{3, 7}, new double[]{0, 1}, 0);
-        System.out.println(
-                Arrays.toString(
-                        new FastGradientDescentMethod(0.00001).findMinimum(f1, new double[]{-1, -2})
-                )
-        );
+//        Random random = new Random();
+//        for (int SIZE = 100; SIZE <= 100000; SIZE *= 10) {
+//            int OBS = 1000;
+//            DoubleStream generator = random.doubles(SIZE, 1, OBS);
+//            double[] a = generator.sorted().toArray();
+//            a[0] = 1;
+//            a[SIZE - 1] = OBS;
+//            Function f = new QuadraticDiagonalFunction(a, new double[SIZE], 0);
+//            double[] x = Arrays.stream(new double[SIZE]).map((d) -> d + 1).toArray();
+//            AbstractGradientMethod method = new ConjugateGradientMethod(0.00001);
+//            method.findMinimum(f, x);
+//            System.out.println(method.counter);
+//        }
     }
 }
