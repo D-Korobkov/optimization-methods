29d2ef07c9643d29d1507a2e9b1189fd6631adb2
Karile Myrr <kirillant2013@gmail.com>
2021-05-31 17:05:56 +0300

    Added starter packages

---
+++ Lab4/src/matrix/LineColumnMatrix.java
@@ -0,0 +1,190 @@
+package matrix;
+
+import SaZhaK.MatrixUtil;
+import interfaces.Function;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+
+/**
+ * Класс, реализующий хранение матриц в разреженно строчно-столбцовом формате
+ */
+public class LineColumnMatrix implements Function {
+
+    /**
+     * Массив al - строки нижнего треуольника без нулей
+     */
+    private double[] al;
+    /**
+     * Массив au - столбцы верхнего треугольника без нулей
+     */
+    private double[] au;
+    /**
+     * Массив d - диагональ матрицы
+     */
+    private double[] d;
+    /**
+     * Массив b - правая часть уравнения
+     */
+    private double[] b;
+    /**
+     * Массив ia - индексный массив для al, au
+     */
+    private int[] ia;
+    /**
+     * Массив ja - массив номеров столбцов (строк) в al (au)
+     */
+    private int[] ja;
+
+    /**
+     * Набор всех файлов для чтения и заполнения класса.
+     */
+    private static final String[] NAME_OF_FILES = {"au.txt", "al.txt", "ia.txt", "d.txt", "ja.txt", "b.txt"};
+
+    /**
+     * Стандартный конструктор.
+     * @param pathOfMatrixAndVector - путь, по которому лежат файлы из {@link #NAME_OF_FILES}
+     */
+    public LineColumnMatrix(final String pathOfMatrixAndVector) {
+        for (final String fileName : NAME_OF_FILES) {
+            try (final BufferedReader reader = Files.newBufferedReader(Path.of(pathOfMatrixAndVector + File.separator + fileName))) {
+                switch (fileName) {
+                    case "au.txt" -> au = Arrays.stream(reader.readLine().split(" ")).mapToDouble(Double::parseDouble).toArray();
+                    case "al.txt" -> al = Arrays.stream(reader.readLine().split(" ")).mapToDouble(Double::parseDouble).toArray();
+                    case "ia.txt" -> ia = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+                    case "ja.txt" -> ja = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+                    case "d.txt" -> d = Arrays.stream(reader.readLine().split(" ")).mapToDouble(Double::parseDouble).toArray();
+                    case "b.txt" -> b = Arrays.stream(reader.readLine().split(" ")).mapToDouble(Double::parseDouble).toArray();
+                }
+            } catch (final IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Размер матрицы
+     * @return рамзер матрицы
+     */
+    public int size() {
+        return d.length;
+    }
+
+    /**
+     * Доступ к элементам матрицы по индексам строки и столбца.
+     * @param i - строка матрицы
+     * @param j - столбец матрицы
+     * @return элемент из матрицы. Аналогично плотной матрице А - A[i][j].
+     */
+    public double getIJ(final int i, final int j) {
+        if (i == j) {
+            return d[i];
+        }
+        if (i > j) {
+            return getLowTriangle(i, j);
+        } else {
+            return getHighTriangle(i, j);
+        }
+    }
+
+    /**
+     * Возвращает элемент из нижнего треугольника без диагонали
+     * @param i - строка
+     * @param j - столбец
+     * @return значение из нижнего треугольника
+     */
+    private double getLowTriangle(final int i, final int j) {
+        return getFromTriangle(i, j, true);
+    }
+
+    /**
+     * Возвращает элемент из верхнего треугольника без диагонали
+     * @param i - строка
+     * @param j - столбец
+     * @return значение из верхнего треугольника
+     */
+    private double getHighTriangle(final int i, final int j) {
+        return getFromTriangle(j, i, false);
+    }
+
+    /**
+     * Абстракция для получения элемента из треугольника вне диагонали.
+     * @param line - строка треугольника
+     * @param indInLine - столбец треугольника, менший чем строка
+     * @param low - флаг, что берут из из нижнего треугольника
+     * @return значение из треугольной матрицы
+     */
+    private double getFromTriangle(final int line, final int indInLine, final boolean low) {
+        if (line <= indInLine) {
+            throw new IllegalArgumentException("Not Triangle indexes line = " + (low ? line : indInLine) + "; column = " + (!low ? line : indInLine) + ";");
+        }
+        final int realInJA = ia[line + 1] - ia[line];
+        int offset = 0;
+        for (; offset < realInJA; offset++) {
+            if (ja[ia[line] + offset] == indInLine) {
+                break;
+            } else if (ja[ia[line] + offset] > indInLine) {
+                return 0;
+            }
+        }
+        if (offset == realInJA) {
+            return 0;
+        }
+        if (low) {
+            return al[ia[line] + offset];
+        } else {
+            return au[ia[line] + offset];
+        }
+    }
+
+    @Override
+    public double run(final double[] x) {
+        final double[] a = multiply(x);
+        final double quad = MatrixUtil.dotProduct(x, a) / 2;
+        final double one = MatrixUtil.dotProduct(b, x);
+
+        return quad - one;
+    }
+
+    @Override
+    public double[] runGradient(final double[] x) {
+        return MatrixUtil.subtract(multiply(x), b);
+    }
+
+    @Override
+    public double[] multiply(final double[] x) {
+        final double[] ans = new double[size()];
+        for (int i = 0; i < size(); i++) {
+            for (int j = 0; j < size(); j++) {
+                ans[i] += getIJ(i, j) * x[j];
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * Получение вектора правой части.
+     * @return вектор b
+     */
+    public double[] getB() {
+        return b;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("LineColumnMatrix{");
+        sb.append(System.lineSeparator());
+        for (int i = 0; i < size(); i++) {
+            for (int j = 0; j < size(); j++) {
+                sb.append(getIJ(i, j)).append(" ");
+            }
+            sb.append(System.lineSeparator());
+        }
+        sb.append("}");
+        return sb.toString();
+    }
+}
---
+++ Lab4/src/matrix/ProfileMatrix.java
@@ -0,0 +1,353 @@
+package matrix;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+
+/**
+ * Class Class of ProfileMatrix realisation
+ */
+public class ProfileMatrix {
+
+    /**
+     * profile rows of lower triangle
+     */
+    public double[] al;
+    /**
+     * profile columns of upper triangle
+     */
+    private double[] au;
+    /**
+     * matrix diagonal
+     */
+    private double[] d;
+
+    /**
+     * index array for al, au
+     */
+    public int[] ia;
+
+    /**
+     * is matrix LU decompositioned
+     */
+    boolean isLU;
+
+    /**
+     * Set of files for reading and setting ProfileMatrix.
+     */
+    private static final String[] NAME_OF_FILES = {"au.txt", "al.txt", "ia.txt", "d.txt"};
+
+
+    /**
+     * Constructor for ProfileMatrix, that reads {@link #readFromPath(String)} it from directory.
+     *
+     * @param pathOfMatrix - path to directory, that contains files for matrix
+     */
+    public ProfileMatrix(final String pathOfMatrix) {
+        this(pathOfMatrix, false);
+    }
+
+    /**
+     * Constructor for ProfileMatrix, that reads {@link #readFromPath(String)} it from directory.
+     * Can specify that matrix is already LU decompositioned
+     *
+     * @param pathOfMatrix - path to directory, that contains files for matrix
+     * @param isLU - is matrix already LU
+     */
+    public ProfileMatrix(final String pathOfMatrix, final boolean isLU) {
+        this.isLU = isLU;
+        readFromPath(pathOfMatrix);
+
+    }
+
+    /**
+     * Reading Matrix from directory. Using following files: au.txt, al.txt, ia.txt, d.txt.
+     *
+     * @param pathOfMatrix - path to directory, that contains files for matrix
+     * @see generator.MatrixGenerator
+     */
+    private void readFromPath(final String pathOfMatrix) {
+        for (final String fileName : NAME_OF_FILES) {
+            try (final BufferedReader reader = Files.newBufferedReader(Path.of(pathOfMatrix + File.separator + fileName))) {
+                switch (fileName) {
+                    case "au.txt" -> au = Arrays.stream(reader.readLine().split(" ")).mapToDouble(Double::parseDouble).toArray();
+                    case "al.txt" -> al = Arrays.stream(reader.readLine().split(" ")).mapToDouble(Double::parseDouble).toArray();
+                    case "ia.txt" -> ia = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+                    case "d.txt" -> d = Arrays.stream(reader.readLine().split(" ")).mapToDouble(Double::parseDouble).toArray();
+                }
+            } catch (final IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Getter for specified element. Works before LU decomposition
+     *
+     * @param i - row
+     * @param j - column
+     * @return - value of ij element
+     */
+    public double getIJ(final int i, final int j) {
+        if (isLU) {
+            throw new IllegalStateException("Matrix is LU. Use getU, getL");
+        }
+        return getIJWithoutException(i, j);
+    }
+
+    /**
+     * Getter for specified element.
+     *
+     * @param i - row
+     * @param j - column
+     * @return - value of ij element
+     */
+    private double getIJWithoutException(final int i, final int j) {
+        if (i == j) {
+            return d[i];
+        } else if (i < j) {
+            return getHighTriangle(i, j);
+        } else {
+            return getLowTriangle(i, j);
+        }
+    }
+
+    /**
+     * Getter of matrix dimension.
+     * @return - size of diagonal(=dimension)
+     */
+    public int size() {
+        return d.length;
+    }
+
+    /**
+     * Getter for specified element from specified profile
+     *
+     * @param i - row if match to upper triangle
+     * @param j - column if match to upper triangle
+     * @param profile - upper or lower a
+     * @return - value of ij element of a
+     */
+    private double profileGet(final int i, final int j, final double[] profile) {
+        if (i == j) {
+            throw new IllegalStateException("Coordinate from diag " + i + " " + j);
+        }
+        if (j > i) {
+            return 0;
+        }
+        final int realCount = ia[i + 1] - ia[i];
+        final int imagineCount = i - realCount;
+        if (j < imagineCount) {
+            return 0;
+        } else {
+            return profile[ia[i] + j - imagineCount];
+        }
+    }
+
+    /**
+     * Getter for specified element from lower triangle
+     *
+     * @param i - row
+     * @param j - column
+     * @return - value of ij element from lower triangle
+     */
+    public double getLowTriangle(final int i, final int j) {
+        return profileGet(i, j, al);
+    }
+
+    /**
+     * Getter for specified element from upper triangle
+     *
+     * @param i - row
+     * @param j - column
+     * @return - value of ij element from upper triangle
+     */
+    public double getHighTriangle(final int i, final int j) {
+        return profileGet(j, i, au);
+    }
+
+    /**
+     * Asserting that matrix is LU.
+     */
+    private void assertLU() {
+        if (!isLU) {
+            throw new IllegalStateException("Matrix isn't in LU");
+        }
+    }
+
+    /**
+     * Getter for specified element from L matrix (A = LU)
+     *
+     * @param i - row
+     * @param j - column
+     * @return - value of ij element from L
+     */
+    public double getL(final int i, final int j) {
+        assertLU();
+        if (i == j) {
+            return d[i];
+        } else {
+            return getLowTriangle(i, j);
+        }
+    }
+
+    /**
+     * Getter for specified element from U matrix (A = LU)
+     *
+     * @param i - row
+     * @param j - column
+     * @return - value of ij element from U
+     */
+    public double getU(final int i, final int j) {
+        assertLU();
+        if (i == j) {
+            return 1;
+        } else {
+            return getHighTriangle(i, j);
+        }
+    }
+
+    /**
+     * Setter of specified profile element for specified profile
+     *
+     * @param i - row if matched to upper triangle
+     * @param j - column if matched to upper triangle
+     * @param value - setting value
+     * @param profile - upper or lower a
+     */
+    private void setProfile(final int i, final int j, final double value, final double[] profile) {
+        final int realCount = ia[i + 1] - ia[i];
+        final int imagineCount = i - realCount;
+        if (j >= imagineCount) {
+            profile[ia[i] + j - imagineCount] = value;
+        }
+    }
+
+    /**
+     * Setter of specified element in L
+     *
+     * @param i - row
+     * @param j - column
+     * @param value - setting value
+     */
+    public void setL(final int i, final int j, final double value) {
+        assertLU();
+        if (i == j) {
+            d[i] = value;
+            return;
+        }
+        if (i < j) {
+            return;
+        }
+        setProfile(i, j, value, al);
+    }
+
+    /**
+     * Setter of specified element in U
+     *
+     * @param i - row
+     * @param j - column
+     * @param value - setting value
+     */
+    public void setU(final int i, final int j, final double value) {
+        assertLU();
+        if (i >= j) {
+            return;
+        }
+        setProfile(j, i, value, au);
+    }
+
+    /**
+     * Decomposition of a matrix into LU
+     */
+    public void changeToLU() {
+        if (isLU) {
+            return;
+        }
+        isLU = true;
+
+        setL(0, 0, getIJWithoutException(0, 0));
+
+        for (int i = 1; i < size(); i++) {
+            //setting L_ij
+            for (int j = 0; j < i; j++) {
+                double substract = 0;
+                for (int k = 0; k < j; k++) {
+                    substract += getL(i, k) * getU(k, j);
+                }
+                setL(i, j, getIJWithoutException(i, j) - substract);
+            }
+
+            //setting U_ji
+            for (int j = 0; j < i; j++) {
+                double substract = 0;
+                for (int k = 0; k < j; k++) {
+                    substract += getL(j, k) * getU(k, i);
+                }
+                setU(j, i, (getIJWithoutException(j, i) - substract) / getL(j, j));
+            }
+
+            //setting L_ii
+            double substract = 0;
+            for (int k = 0; k < i; k++) {
+                substract += getL(i, k) * getU(k, i);
+            }
+            setL(i, i, getIJWithoutException(i, i) - substract);
+
+            //setting U_ii
+            setU(i, i, 1);
+        }
+    }
+
+    /**
+     * @return - string representation of matrix
+     */
+    @Override
+    public String toString() {
+        return "Matrix{" +
+                "al=" + Arrays.toString(al) +
+                ", au=" + Arrays.toString(au) +
+                ", d=" + Arrays.toString(d) +
+                ", ia=" + Arrays.toString(ia) +
+                '}';
+    }
+
+    /**
+     * Writing matrix in "square" format to System.out
+     */
+    public void showByGetters() {
+        for (int i = 0; i < size(); i++) {
+            for (int j = 0; j < size(); j++) {
+                System.out.print(getIJ(i, j) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * Writing L part os Matrix in "square" format to System.out
+     */
+    public void showL() {
+        for (int i = 0; i < size(); i++) {
+            for (int j = 0; j < size(); j++) {
+                System.out.print(getL(i, j) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * Writing U part os Matrix in "square" format to System.out
+     */
+    public void showU() {
+        for (int i = 0; i < size(); i++) {
+            for (int j = 0; j < size(); j++) {
+                System.out.print(getU(i, j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
