d79ac08b551f8a9d3324b69d87daef8803bf47b0
ADevil000 <artem.kolosov2000@yandex.ru>
2021-06-03 11:35:10 +0300

    less calculations

--- Lab4/src/main/java/TestFunctions/Function2_4.java
+++ Lab4/src/main/java/TestFunctions/Function2_4.java
@@ -7,31 +7,33 @@ import static TestFunctions.Helper.*;
 public class Function2_4 implements Function {
     @Override
     public double run(double[] x) {
-        return 100 - 2 / zn1(x) - 1 / zn2(x);
+        return 100 - 2 / functionZn1(x) - 1 / functionZn2(x);
     }
 
-    private double zn2(double[] x) {
+    private double functionZn2(double[] x) {
         return (1 + sqr((x[0] - 2) / 2) + sqr((x[1] - 1) / 3));
     }
 
-    private double zn1(double[] x) {
+    private double functionZn1(double[] x) {
         return (1 + sqr((x[0] - 1) / 2) + sqr((x[1] - 1) / 3));
     }
 
     @Override
     public double[] runGradient(double[] x) {
         double[] ans = new double[2];
-        ans[0] = findGrad1(x);
-        ans[1] = findGrad2(x);
+        double zn1 = gradZn1(x);
+        double zn2 = gradZn2(x);
+        ans[0] = findGrad1(x, zn1, zn2);
+        ans[1] = findGrad2(x, zn1, zn2);
         return ans;
     }
 
-    private double findGrad2(double[] x) {
-        return 2 * (x[1] - 1) / (9 * gradZn1(x)) + 4 * (x[1] - 1) / (9 * gradZn2(x));
+    private double findGrad2(double[] x, double zn1, double zn2) {
+        return 2 * (x[1] - 1) / (9 * zn1) + 4 * (x[1] - 1) / (9 * zn2);
     }
 
-    private double findGrad1(double[] x) {
-        return (x[0] - 2) / (2 * gradZn1(x)) + (x[0] - 1) / gradZn2(x);
+    private double findGrad1(double[] x, double zn1, double zn2) {
+        return (x[0] - 2) / (2 * zn1) + (x[0] - 1) / zn2;
     }
 
     private double gradZn1(double[] x) {
@@ -58,26 +60,28 @@ public class Function2_4 implements Function {
     @Override
     public double[][] runHessian(double[] x) {
         double[][] H = new double[2][2];
-        H[0][0] = findH11(x);
-        H[0][1] = H[1][0] = findH12(x);
-        H[1][1] = findH22(x);
+        double zn1 = differentialZn1(x);
+        double zn2 = differentialZn2(x);
+        H[0][0] = findH11(x, zn1, zn2);
+        H[0][1] = H[1][0] = findH12(x, zn1, zn2);
+        H[1][1] = findH22(x, zn1, zn2);
         return H;
     }
 
-    private double findH22(double[] x) {
-        return -8 * sqr(x[1] - 1) / (81 * cube(differentialZn1(x)))
-                -16 * sqr(x[1] - 1) / (81 * cube(differentialZn2(x)))
-                + 2 / (9 * sqr(differentialZn1(x))) + 4 / (9 * sqr(differentialZn2(x)));
+    private double findH22(double[] x, double zn1, double zn2) {
+        return -8 * sqr(x[1] - 1) / (81 * cube(zn1))
+                -16 * sqr(x[1] - 1) / (81 * cube(zn2))
+                + 2 / (9 * sqr(zn1)) + 4 / (9 * sqr(zn2));
     }
 
-    private double findH12(double[] x) {
-        return -2 * (x[0] - 2) * (x[1] - 1) / (9 * cube(differentialZn1(x)))
-                - 4 * (x[0] - 1) * (x[1] - 1) / (9 * cube(differentialZn2(x)));
+    private double findH12(double[] x, double zn1, double zn2) {
+        return -2 * (x[0] - 2) * (x[1] - 1) / (9 * cube(zn1))
+                - 4 * (x[0] - 1) * (x[1] - 1) / (9 * cube(zn2));
     }
 
-    private double findH11(double[] x) {
-        return -sqr(x[0] - 2) / (2 * cube(differentialZn1(x)))
-                - 2 * (sqr(x[0] - 1) / (2 * cube(differentialZn2(x))) - 1 / (2 * sqr(differentialZn2(x))))
-                + 1 / (2 * sqr(differentialZn1(x)));
+    private double findH11(double[] x, double zn1, double zn2) {
+        return -sqr(x[0] - 2) / (2 * cube(zn1))
+                - 2 * (sqr(x[0] - 1) / (2 * cube(zn2)) - 1 / (2 * sqr(zn2)))
+                + 1 / (2 * sqr(zn1));
     }
 }
